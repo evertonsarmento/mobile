730e7bc90bfae53cbf81a85871a343419f8f7ff7
evertonsarmento <evertonsarmento@gmail.com>
2017-05-08 12:21:48 -0300

    2

--- MyApplication/app/src/main/java/com/example/sala01/myapplication/TesteAPP.java
+++ MyApplication/app/src/main/java/com/example/sala01/myapplication/TesteAPP.java
@@ -2,6 +2,9 @@ package com.example.sala01.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TesteAPP extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class TesteAPP extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teste_app);
+
+        final TextView txtDinamico = (TextView)findViewById(R.id.txtDinamico);
+        Button btClique = (Button) findViewById(R.id.btClique);
+
+        btClique.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtDinamico.setText("Fui Clicado");
+            }
+
+        });
     }
+
+
 }
