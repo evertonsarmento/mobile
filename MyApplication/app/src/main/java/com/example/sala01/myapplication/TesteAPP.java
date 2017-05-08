d935d3e7c72fc894886264a151d5949845bc898b
evertonsarmento <evertonsarmento@gmail.com>
2017-05-08 12:00:10 -0300

    primeiro app

---
+++ MyApplication/app/src/main/java/com/example/sala01/myapplication/TesteAPP.java
@@ -0,0 +1,13 @@
+package com.example.sala01.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TesteAPP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teste_app);
+    }
+}
